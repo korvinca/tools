22c9e8d19e8692150a5f5f9380acfe4a8d397ca2
Ivan Korolevskiy <ivan.korolevskiy@gmail.com>
2023-10-21 18:59:41 -0700

    add test

--- doc/code_snippets/test.py
+++ doc/code_snippets/test.py
@@ -1,12 +1,21 @@
-known_worlds = ['trace']
+def update_string(url, a, b):
+    res = url + '{' + a + '}search{' + b + '}'
+    print(res)
 
-know_l = ['t','r','a','c','e']
-new_in = 'react' #get_anagrams('nuno') # --> []
-p =[]
-for i in know_l: # i = 't'
-    for j in new_in: # new_in.split() = ['n', 'u', 'n', 'o'] ; j = 'n'
-        if i == j:
-            p = p.insert(i) # p = []
-    if p == known_worlds[0]: # 
-        known_words.append(new_in)
-print(known_worlds[0])
+# known_worlds = ['trace']
+
+# know_l = ['t','r','a','c','e']
+# new_in = 'react' #get_anagrams('nuno') # --> []
+# p =[]
+# for i in know_l: # i = 't'
+#     for j in new_in: # new_in.split() = ['n', 'u', 'n', 'o'] ; j = 'n'
+#         if i == j:
+#             p = p.insert(i) # p = []
+#     if p == known_worlds[0]: # 
+#         known_words.append(new_in)
+# print(known_worlds[0])
+
+in_url = "https://www.test.com/api&search"
+a = "one"
+b = "two"
+update_string(in_url,a,b)
