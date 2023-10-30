f8eac99996ba5542514ce721a9c2b6b67813500e
Ivan Korolevskiy <ivan.korolevskiy@gmail.com>
2023-10-29 20:18:42 -0700

    fibonacci

---
+++ doc/code_snippets/examples/fibonacci.py
@@ -0,0 +1,41 @@
+# Option 1 for
+def fib(n):
+    a = 0
+    b = 1
+    for __ in range(n):
+        a, b = b, a + b
+    return a
+
+res = [0]
+count = 1
+while count <= 10:
+    res.append(fib(count))
+    count += 1
+print(res)
+
+# Option 2 while
+res = [0]
+fib1 = 0
+fib2 = 1
+i = 0
+n = 10
+while i < n:
+    fib_sum = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib_sum
+    i += 1
+    res. append(fib1)
+print(res)
+
+# Option 3 recrusia
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+res = [0]
+count = 1
+while count <= 10:
+    res.append(fibonacci(count))
+    count += 1
+
+print(res)
