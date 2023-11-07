efcedc630b027a596faa9c3290e39da45a150c47
Ivan Korolevskiy <ivan.koroelvskiy@gmail.com>
2023-11-07 14:16:04 -0800

    add re

---
+++ doc/code_snippets/examples/regex.py
@@ -0,0 +1,12 @@
+import re
+
+string = "The quick brown fox jumps over the lazy dog."
+
+# re.sub(pattern, replace, string, count=0, flags=0)
+# Replace all occurrences of "the" with "a" using a regular expression
+
+new_string = re.sub("the", "a", string)
+print(new_string)
+
+new_string = re.sub(r"\b[tT]\w*e\b", "a", string)
+print(new_string)
