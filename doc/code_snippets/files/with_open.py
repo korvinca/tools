01fac21d48ded660fd62510cc88407fb4a8c17ae
Ivan Korolevskiy <ivan.koroelvskiy@gmail.com>
2023-11-08 12:05:42 -0800

    add json parcing

--- doc/code_snippets/files/with_open.py
+++ doc/code_snippets/files/with_open.py
@@ -1,13 +1,17 @@
-with open("data.json") as mfile:
-    print(mfile.read())
+import json
+with open("data1.json", "r") as mfile:
+    content = mfile.read()
+    print(type(content))
+data = json.loads(content)
+print(type(data))
+print(str(data))
 
+# with open("data.json") as my_file:
+#     for line in my_file:
+#         if line:
+#             print(line)
 
-with open("data.json") as my_file:
-    for line in my_file:
-        if line:
-            print(line)
-
-fname = open('mbox-short.txt', 'r')
-for line in fname:
-    print(line, end='')
-# print("\n")
+# fname = open('mbox-short.txt', 'r')
+# for line in fname:
+#     print(line, end='')
+# # print("\n")
