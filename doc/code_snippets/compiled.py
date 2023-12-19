7d6dba8b85ca7a45b40bf4f1d099ed6d058080d3
Ivan Korolevskiy <ivan.koroelvskiy@gmail.com>
2023-12-19 11:55:19 -0800

    update

--- doc/code_snippets/compiled.py
+++
@@ -1,4 +0,0 @@
-print("i am learning python")
-print("i am enjoying it")
-
-#python -m py_compile compiled.py
--- doc/code_snippets/strings/split_line.py
+++ doc/code_snippets/strings/split_line.py
@@ -1,17 +1,27 @@
-fname = raw_input('Enter the file name: ')
+# fname = raw_input('Enter the file name: ')
 
-try:
-	fhand = open(fname)
-except:
-	print ('File cannot be opened:'), fname
-	exit()
-counts = dict()
+# try:
+# 	fhand = open(fname)
+# except:
+# 	print ('File cannot be opened:'), fname
+# 	exit()
+# counts = dict()
 
-for line in fhand:
-	words = line.split()
-	for word in words:
-		if word not in counts:
-			counts[word] = 1
-		else:
-			counts[word] += 1
-print (counts)
+# for line in fhand:
+# 	words = line.split()
+# 	for word in words:
+# 		if word not in counts:
+# 			counts[word] = 1
+# 		else:
+# 			counts[word] += 1
+# print (counts)
+
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = "smlit me please"
+    result = split_and_join(line)
+    print(result)
